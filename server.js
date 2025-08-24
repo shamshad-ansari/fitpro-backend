32d57e19d9b75e79ee09edba9a89cfe168238f31
Shamshad Ansari <149064103+shamshad-ansari@users.noreply.github.com>
2025-08-24 16:08:29 -0500

    Centralized environment config

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import app from "./app.js";
+import { env } from "./config/env.js";
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(env.port, () => {
+  console.log(`🚀 Server running on http://localhost:${env.port}`);
 });
